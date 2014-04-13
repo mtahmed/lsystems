b29cabb2c83bd883b6c4e07a05ed3c96c57784c1
Muhammad Tauqir Ahmad <muhammad.tauqir.ahmad@gmail.com>
2014-04-12 22:18:43 -0400

    Add the basic LSystem class.

---
+++ lsystem.py
@@ -0,0 +1,45 @@
+class LSystem:
+    '''Represents an L-System with intial state, production rules and alphabet.
+    '''
+    def __init__(self, initial, productions, alphabet=None):
+        self.initial = initial
+        self.productions = productions
+        self.alphabet = alphabet
+
+        self.state = self.initial
+
+        return
+
+    def step(self):
+        '''Step once through the LSystem.
+
+        In other words, apply the production rules once to the current state.
+        '''
+        new_symbols = []
+        for symbol in self.state:
+            try:
+                new_symbol = self.productions[symbol]
+                new_symbols.append(new_symbol)
+            except KeyError:
+                pass
+
+        self.state = ''.join(new_symbols)
+
+        return
+
+    def step_n(self, n):
+        '''Step n times through the LSystem.
+
+        In other words, invoke the step function n times.
+        '''
+        for _ in range(n):
+            self.step()
+
+        return
+
+    def draw(self):
+        '''Draw the current state.
+
+        The default is just to print the state string.
+        '''
+        print(self.state)
