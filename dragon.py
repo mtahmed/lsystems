09264fe6c841235b2434c42b20450841ca8f175d
Syed Zahir Bokhari <szbokhar@Syeds-MacBook-Pro.local>
2014-04-14 15:30:33 -0400

    Dragon segment length now scales down with respect to the number of iterations, with min on 1.

--- dragon.py
+++ dragon.py
@@ -1,10 +1,11 @@
 from  lsystem import *
 import turtle
 import sys
+import math
 
 class Dragon_LSystem(LSystem):
     def __init__(self):
-        super(Dragon_LSystem, self).__init__("FX", {"X":"X+YF", "Y":"FX-Y"})
+        super(Dragon_LSystem, self).__init__("LFX", {"X":"X+YF", "Y":"FX-Y", "L":"CL"})
 
     def draw(self):
         super(Dragon_LSystem, self).draw()
@@ -16,18 +17,24 @@ class Dragon_LSystem(LSystem):
 
         self.turtle = turtle.Turtle()
         self.turtle.shape('blank')
-        self.turtle.tracer(8,25)
+        turtle.tracer(int(sys.argv[2]),25)
         t = self.turtle
         t.reset()
 
-        t.left(90)
+        t.penup()
+        t.setpos(-200,0)
+        t.pendown()
+        i = 200.0
         for c in self.state:
             if c == "F":
-                t.forward(10)
+                t.forward(math.ceil(i))
             elif c == "+":
                 t.right(90)
             elif c == "-":
                 t.left(90)
+            elif c == "C":
+                i = i/math.sqrt(2)
+                t.left(45)
 
         wn.exitonclick()
 
